e6843b1511ecd31bb22367a4282c4e534e8875c9
daiwk <daiwk@foxmail.com>
2018-08-26 05:20:21 +0800

    xush

--- python/gt/framework/tools/log.py
+++ python/gt/framework/tools/log.py
@@ -40,29 +40,29 @@ def init_log(file, level=logging.INFO, when="D", backup=7,
     init_log - initialize log module
 
     Args:
-      log_path      - Log file path prefix.
-                      Log data will go to two files: log_path.log and log_path.log.wf
-                      Any non-exist parent directories will be created automatically
-      level         - msg above the level will be displayed
-                      DEBUG < INFO < WARNING < ERROR < CRITICAL
-                      the default value is logging.INFO
-      when          - how to split the log file by time interval
-                      'S' : Seconds
-                      'M' : Minutes
-                      'H' : Hours
-                      'D' : Days
-                      'W' : Week day
-                      default value: 'D'
-      format        - format of the log
-                      default format:
-                      %(levelname)s: %(asctime)s: %(filename)s:%(lineno)d * %(thread)d %(message)s
-                      INFO: 12-09 18:02:42: log.py:40 * 139814749787872 HELLO WORLD
-      backup        - how many backup file to keep
-                      default value: 7
+        + log_path: Log file path prefix.
+            Log data will go to two files: log_path.log and log_path.log.wf
+            Any non-exist parent directories will be created automatically
+        + level: msg above the level will be displayed
+            DEBUG < INFO < WARNING < ERROR < CRITICAL
+            the default value is logging.INFO
+        + when: how to split the log file by time interval
+            + 'S' : Seconds
+            + 'M' : Minutes
+            + 'H' : Hours
+            + 'D' : Days
+            + 'W' : Week day
+            default value: 'D'
+        + format: format of the log
+            default format:
+            %(levelname)s: %(asctime)s: %(filename)s:%(lineno)d * %(thread)d %(message)s
+            INFO: 12-09 18:02:42: log.py:40 * 139814749787872 HELLO WORLD
+        + backup: how many backup file to keep
+            default value: 7
 
     Raises:
-        OSError: fail to create log directories
-        IOError: fail to open log file
+        + OSError: fail to create log directories
+        + IOError: fail to open log file
     '''
     formatter = logging.Formatter(format, datefmt)
     logger = logging.getLogger()
